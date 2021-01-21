6689fa25e9c79a0763a0fc737d57afcb059e3356
babaralishah <babar.bscs3261@iu.edu.pk>
2021-01-21 11:20:56 +0500

    latest pushing

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const userRouter = require('./resources/User/user.router');
+const express = require("express");
+const userRouter = require("./resources/User/user.router");
 //
 const restRouter = express.Router();
 /* 
 Resources of API 
 */
-restRouter.use('/users', userRouter);
-
+restRouter.use("/users", userRouter);
 
 module.exports = restRouter;
